b0d55f38d60413380fad1e6953df4ea39c93e5a3
KlaporeDevs <verbojanrich20@gmail.com>
2025-07-31 23:35:30 +0800

    Gift

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something For You. Mahal Ko'.split('')
+const text = 'I Have Something For You. Mahal Ko❤️'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day Sayo Pinakamamahal kong Binibini. 💓').split('')
+    const titles = ('Happy Girlfriends Day Sayo Aking Binibini.💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
